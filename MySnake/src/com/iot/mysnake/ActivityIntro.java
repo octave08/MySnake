f47d9b5399b5c9606939e385ee98e3418869612b
octave08 <octave08sc@naver.com>
2014-10-09 19:55:31 +0900

    + update IntroActivity....

--- MySnake/src/com/iot/mysnake/ActivityIntro.java
+++ MySnake/src/com/iot/mysnake/ActivityIntro.java
@@ -1,6 +1,7 @@
 package com.iot.mysnake;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
@@ -9,11 +10,13 @@ import android.view.Window;
 import android.widget.ImageButton;
 
 
-public class IntroAtivity extends Activity {
+public class ActivityIntro extends Activity {
 
 	private ImageButton btnStart;
 	private ImageButton btnBackFromStage;
+	private ImageButton	btnGoStage1;
 	private ImageButton btnBackFromInfinity;
+	private ImageButton btnGoInfinity;
 	
 	private ImageButton btnMode;
 	private ImageButton btnBackFromMode;
@@ -80,6 +83,20 @@ public class IntroAtivity extends Activity {
 			
 		});
 		
+		btnGoStage1 = (ImageButton)findViewById(R.id.btnStage1);
+		btnGoStage1.setOnClickListener(new OnClickListener()
+		{
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(ActivityIntro.this, ActivityPlay.class);
+				intent.putExtra("gamemode", m_gameMode);
+				startActivityForResult(intent, -1);
+			}
+			
+		});
+		
 		btnBackFromInfinity = (ImageButton)findViewById(R.id.btnBackFromInfinity);
 		btnBackFromInfinity.setOnClickListener(new OnClickListener() 
 		{
@@ -88,6 +105,20 @@ public class IntroAtivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 					linearInfinity.setVisibility(View.INVISIBLE);
+			}
+			
+		});
+		
+		btnGoInfinity = (ImageButton)findViewById(R.id.btnInfinity);
+		btnGoInfinity.setOnClickListener(new OnClickListener()
+		{
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(ActivityIntro.this, ActivityPlay.class);
+				intent.putExtra("gamemode", m_gameMode);
+				startActivityForResult(intent, -1);
 			}
 			
 		});
---
+++ MySnake/src/com/iot/mysnake/ActivityPlay.java
@@ -0,0 +1,29 @@
+package com.iot.mysnake;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+public class ActivityPlay extends Activity {
+	
+	private int m_gameMode;
+	private final static int STAGE 		= 1;
+	private final static int INFINITY 	= 2;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.layout_play);
+		
+		Intent intent = getIntent();
+		m_gameMode = intent.getIntExtra("gamemode", STAGE);
+		
+	}
+	
+	
+
+}
